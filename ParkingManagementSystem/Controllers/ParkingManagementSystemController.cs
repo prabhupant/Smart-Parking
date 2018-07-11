20b0b88926c9f7bbe9052d4b1dbd6cc96c9fdd0c
Prabhu Pant <prabhupant09@gmail.com>
2018-07-11 23:21:00 +0530

    Initialize the repository. Create basic website.

---
+++ ParkingManagementSystem/Controllers/ParkingManagementSystemController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ParkingManagementSystem.Controllers
+{
+    public class ParkingManagementSystemController : Controller
+    {
+        // GET: ParkingManagementSystem
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult ParkingSpotForm()
+        {
+            return View();
+        }
+    }
+
+    
+}
---
+++ ParkingManagementSystem/Models/UserInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParkingManagementSystem.Models
+{
+    public class UserInfo
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string VehicleNumber { get; set; }
+        public string UID { get; set; }
+    }
+}
