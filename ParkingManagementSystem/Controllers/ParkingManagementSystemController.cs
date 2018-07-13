28a572d45a54afe9f151dad9ff2d26dd7d36c308
Prabhu Pant <prabhupant09@gmail.com>
2018-07-13 11:14:09 +0530

    Update LogIn and LogOut form.

--- ParkingManagementSystem/Controllers/ParkingManagementSystemController.cs
+++ ParkingManagementSystem/Controllers/ParkingManagementSystemController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ParkingManagementSystem.Models;
 
 namespace ParkingManagementSystem.Controllers
 {
@@ -14,9 +15,28 @@ namespace ParkingManagementSystem.Controllers
             return View();
         }
 
+        [HttpGet]
         public ViewResult ParkingSpotForm()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult ParkingSpotForm(UserInfo userInfo)
+        {
+            return View("Thanks", userInfo);
+        }
+
+        [HttpGet]
+        public ViewResult CheckOutForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult CheckOutForm(UserInfo userInfo)
+        {
+            return View("Thanks for coming!", userInfo);
         }
     }
 
--- ParkingManagementSystem/Models/UserInfo.cs
+++ ParkingManagementSystem/Models/UserInfo.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ParkingManagementSystem.Models
 {
     public class UserInfo
     {
+        [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number")]
         public string Phone { get; set; }
+        [Required(ErrorMessage = "Please enter your vehicle number")]
         public string VehicleNumber { get; set; }
+        [Required(ErrorMessage = "Please enter your UID")]
         public string UID { get; set; }
+        
+        public string ParkingSlotNumber { get; set; }
     }
 }
