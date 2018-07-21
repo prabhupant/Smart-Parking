5c513a0dc191e5c18d5ca236dd01774864c1499d
Prabhu Pant <prabhupant09@gmail.com>
2018-07-21 18:22:45 +0530

    Improvised About and Contact Us pages

--- ParkingManagementSystem/Scripts/scipt.js
+++ ParkingManagementSystem/Scripts/scipt.js
@@ -1,209 +1,263 @@
 ﻿function clickFunction(id) {
     document.getElementById(id).style.backgroundColor = "red";
     localStorage.setItem(id, "red");
+
+    document.getElementById(id).setAttribute("value", "change");
+
 }
 
 function onload_function() {
     
+    
 
     if (localStorage.getItem("ps1") !== null) {
         document.getElementById("ps1").style.backgroundColor = "red";
+        document.getElementById("ps1").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps2") !== null) {
         document.getElementById("ps2").style.backgroundColor = "red";
+        document.getElementById("ps2").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps3") !== null) {
         document.getElementById("ps3").style.backgroundColor = "red";
+        document.getElementById("ps3").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps4") !== null) {
         document.getElementById("ps4").style.backgroundColor = "red";
+        document.getElementById("ps4").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps5") !== null) {
         document.getElementById("ps5").style.backgroundColor = "red";
+        document.getElementById("ps5").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps6") !== null) {
         document.getElementById("ps6").style.backgroundColor = "red";
+        document.getElementById("ps6").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps7") !== null) {
         document.getElementById("ps7").style.backgroundColor = "red";
+        document.getElementById("ps7").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps8") !== null) {
         document.getElementById("ps8").style.backgroundColor = "red";
+        document.getElementById("ps8").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps9") !== null) {
         document.getElementById("ps9").style.backgroundColor = "red";
+        document.getElementById("ps9").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps10") !== null) {
         document.getElementById("ps10").style.backgroundColor = "red";
+        document.getElementById("ps10").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps11") !== null) {
         document.getElementById("ps11").style.backgroundColor = "red";
+        document.getElementById("ps11").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps12") !== null) {
         document.getElementById("ps12").style.backgroundColor = "red";
+        document.getElementById("ps12").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps13") !== null) {
         document.getElementById("ps13").style.backgroundColor = "red";
+        document.getElementById("ps13").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps14") !== null) {
         document.getElementById("ps14").style.backgroundColor = "red";
+        document.getElementById("ps14").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps15") !== null) {
         document.getElementById("ps15").style.backgroundColor = "red";
+        document.getElementById("ps15").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps16") !== null) {
         document.getElementById("ps16").style.backgroundColor = "red";
+        document.getElementById("ps16").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps17") !== null) {
         document.getElementById("ps17").style.backgroundColor = "red";
+        document.getElementById("ps17").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps18") !== null) {
         document.getElementById("ps18").style.backgroundColor = "red";
+        document.getElementById("ps18").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps19") !== null) {
         document.getElementById("ps19").style.backgroundColor = "red";
+        document.getElementById("ps19").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps20") !== null) {
         document.getElementById("ps20").style.backgroundColor = "red";
+        document.getElementById("ps20").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps21") !== null) {
         document.getElementById("ps21").style.backgroundColor = "red";
+        document.getElementById("ps21").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps22") !== null) {
         document.getElementById("ps22").style.backgroundColor = "red";
+        document.getElementById("ps22").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps23") !== null) {
         document.getElementById("ps23").style.backgroundColor = "red";
+        document.getElementById("ps23").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps24") !== null) {
         document.getElementById("ps24").style.backgroundColor = "red";
+        document.getElementById("ps24").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps25") !== null) {
         document.getElementById("ps25").style.backgroundColor = "red";
+        document.getElementById("ps25").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps26") !== null) {
         document.getElementById("ps26").style.backgroundColor = "red";
+        document.getElementById("ps26").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps27") !== null) {
         document.getElementById("ps27").style.backgroundColor = "red";
+        document.getElementById("ps27").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps28") !== null) {
         document.getElementById("ps28").style.backgroundColor = "red";
+        document.getElementById("ps28").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps29") !== null) {
         document.getElementById("ps29").style.backgroundColor = "red";
+        document.getElementById("ps29").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps30") !== null) {
         document.getElementById("ps30").style.backgroundColor = "red";
+        document.getElementById("ps30").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps31") !== null) {
         document.getElementById("ps31").style.backgroundColor = "red";
+        document.getElementById("ps31").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps32") !== null) {
         document.getElementById("ps32").style.backgroundColor = "red";
+        document.getElementById("ps32").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps33") !== null) {
         document.getElementById("ps33").style.backgroundColor = "red";
+        document.getElementById("ps33").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps34") !== null) {
         document.getElementById("ps34").style.backgroundColor = "red";
+        document.getElementById("ps34").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps35") !== null) {
         document.getElementById("ps35").style.backgroundColor = "red";
+        document.getElementById("ps35").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps36") !== null) {
         document.getElementById("ps36").style.backgroundColor = "red";
+        document.getElementById("ps36").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps37") !== null) {
-        document.getElementById("ps1").style.backgroundColor = "red";
+        document.getElementById("ps37").style.backgroundColor = "red";
+        document.getElementById("ps37").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps38") !== null) {
         document.getElementById("ps38").style.backgroundColor = "red";
+        document.getElementById("ps38").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps39") !== null) {
         document.getElementById("ps39").style.backgroundColor = "red";
+        document.getElementById("ps39").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps40") !== null) {
         document.getElementById("ps40").style.backgroundColor = "red";
+        document.getElementById("ps40").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps41") !== null) {
         document.getElementById("ps41").style.backgroundColor = "red";
+        document.getElementById("ps41").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps42") !== null) {
         document.getElementById("ps42").style.backgroundColor = "red";
+        document.getElementById("ps42").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps43") !== null) {
         document.getElementById("ps43").style.backgroundColor = "red";
+        document.getElementById("ps43").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps44") !== null) {
         document.getElementById("ps44").style.backgroundColor = "red";
+        document.getElementById("ps44").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps45") !== null) {
         document.getElementById("ps45").style.backgroundColor = "red";
+        document.getElementById("ps45").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps46") !== null) {
         document.getElementById("ps46").style.backgroundColor = "red";
+        document.getElementById("ps46").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps47") !== null) {
         document.getElementById("ps47").style.backgroundColor = "red";
+        document.getElementById("ps47").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps48") !== null) {
         document.getElementById("ps48").style.backgroundColor = "red";
+        document.getElementById("ps48").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps49") !== null) {
         document.getElementById("ps49").style.backgroundColor = "red";
+        document.getElementById("ps49").setAttribute("disabled", "disabled");
     }
 
     if (localStorage.getItem("ps50") !== null) {
         document.getElementById("ps50").style.backgroundColor = "red";
+        document.getElementById("ps50").setAttribute("disabled", "disabled");
     }
 
 }
@@ -216,3 +270,4 @@ function remove_tag(slotNumber) {
     var id = "ps" + slotNumber;
     localStorage.removeItem(id);
 }
+
